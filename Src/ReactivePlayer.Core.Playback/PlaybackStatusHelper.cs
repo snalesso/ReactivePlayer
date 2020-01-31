d63ac9f580b4db6bde504f833934c7ce2a6d2052
Sergio <nalesso.sergio@gmail.com>
2020-01-31 19:00:39 +0100

    optimized PlaybackStatusHelper

--- Src/ReactivePlayer.Core.Playback/PlaybackStatusHelper.cs
+++ Src/ReactivePlayer.Core.Playback/PlaybackStatusHelper.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,41 +9,33 @@ namespace ReactivePlayer.Core.Playback
 {
     public static class PlaybackStatusHelper // TODO: lambda/extension methods + inlining?
     {
-        public static readonly PlaybackStatus[] CanLoadPlaybackStatuses =
-            {
-                PlaybackStatus.None,
-                PlaybackStatus.PlayedToEnd,
-                PlaybackStatus.ManuallyInterrupted,
-                PlaybackStatus.Exploded
-            };
-        public static readonly PlaybackStatus[] CanPlayPlaybackStatuses =
-            {
-                PlaybackStatus.Loaded
-            };
-        public static readonly PlaybackStatus[] CanPausePlaybackStatuses =
-            {
-                PlaybackStatus.Playing
-            };
-        public static readonly PlaybackStatus[] CanResumePlaybackStatuses =
-            {
-                PlaybackStatus.Paused
-            };
-        public static readonly PlaybackStatus[] CanStopPlaybackStatuses =
-            {
-                PlaybackStatus.Playing,
-                PlaybackStatus.Paused
-            };
-        public static readonly PlaybackStatus[] CanSeekPlaybackStatuses =
-            {
-                PlaybackStatus.Loaded,
-                PlaybackStatus.Playing,
-                PlaybackStatus.Paused
-            };
-        public static readonly PlaybackStatus[] StoppedPlaybackStatuses =
-            {
-                PlaybackStatus.PlayedToEnd,
-                PlaybackStatus.ManuallyInterrupted,
-                PlaybackStatus.Exploded
-            };
+        public static readonly IImmutableSet<PlaybackStatus> LoadablePlaybackStatuses = ImmutableHashSet.Create(
+            PlaybackStatus.None,
+            PlaybackStatus.PlayedToEnd,
+            PlaybackStatus.ManuallyInterrupted,
+            PlaybackStatus.Exploded);
+
+        public static readonly IImmutableSet<PlaybackStatus> PlayablePlaybackStatuses = ImmutableHashSet.Create(
+            PlaybackStatus.Loaded);
+
+        public static readonly IImmutableSet<PlaybackStatus> PausablePlaybackStatuses = ImmutableHashSet.Create(
+            PlaybackStatus.Playing);
+
+        public static readonly IImmutableSet<PlaybackStatus> ResumablePlaybackStatuses = ImmutableHashSet.Create(
+            PlaybackStatus.Paused);
+
+        public static readonly IImmutableSet<PlaybackStatus> StoppablePlaybackStatuses = ImmutableHashSet.Create(
+            PlaybackStatus.Playing,
+            PlaybackStatus.Paused);
+
+        public static readonly IImmutableSet<PlaybackStatus> SeekablePlaybackStatuses = ImmutableHashSet.Create(
+            PlaybackStatus.Loaded,
+            PlaybackStatus.Playing,
+            PlaybackStatus.Paused);
+
+        public static readonly IImmutableSet<PlaybackStatus> StoppedPlaybackStatuses = ImmutableHashSet.Create(
+            PlaybackStatus.PlayedToEnd,
+            PlaybackStatus.ManuallyInterrupted,
+            PlaybackStatus.Exploded);
     }
 }
